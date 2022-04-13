b3359487d8e1eb332fe8d8539062642a940eb95a
Li-Haowei <haoweilee666@gmail.com>
2022-04-12 22:50:36 -0400

    implementing message ui

--- app/src/main/java/com/example/androidfirebasechat/MainActivity.java
+++ app/src/main/java/com/example/androidfirebasechat/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidfirebasechat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -9,14 +11,18 @@ public class MainActivity extends AppCompatActivity {
     private String mobile;
     private String email;
     private String name;
+    private RecyclerView messagesRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        messagesRecyclerView = findViewById(R.id.messagesRecyclerView);
         //get intent data from Register.class activity
         mobile = getIntent().getStringExtra("mobile");
         email = getIntent().getStringExtra("email");
         name = getIntent().getStringExtra("name");
+
+        messagesRecyclerView.setHasFixedSize(true);
+        messagesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
