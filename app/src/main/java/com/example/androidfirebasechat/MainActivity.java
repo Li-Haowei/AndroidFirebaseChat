5e9d7ded8afe1f1da28e5d703ef90099d9f60bdf
Li-Haowei <haoweilee666@gmail.com>
2022-04-12 22:16:43 -0400

    implementing chap interface

--- app/src/main/java/com/example/androidfirebasechat/MainActivity.java
+++ app/src/main/java/com/example/androidfirebasechat/MainActivity.java
@@ -6,10 +6,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String mobile; String email; String name;
+    private String mobile;
+    private String email;
+    private String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get intent data from Register.class activity
+        mobile = getIntent().getStringExtra("mobile");
+        email = getIntent().getStringExtra("email");
+        name = getIntent().getStringExtra("name");
     }
 }
