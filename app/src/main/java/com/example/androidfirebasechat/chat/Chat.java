fbf2a5d4180d482c42c54c41242c44d9ef700d3c
Li-Haowei <haoweilee666@gmail.com>
2022-04-14 15:40:52 -0400

    chat now functional

--- app/src/main/java/com/example/androidfirebasechat/chat/Chat.java
+++ app/src/main/java/com/example/androidfirebasechat/chat/Chat.java
@@ -2,6 +2,8 @@ package com.example.androidfirebasechat.chat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -19,6 +21,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Chat extends AppCompatActivity {
@@ -28,7 +37,11 @@ public class Chat extends AppCompatActivity {
     private ImageView sendBtn;
     private CircleImageView profilePic;
     private String chatKey ;
+    private final List<ChatList> chatLists = new ArrayList<>();
+    private ChatAdapter chatAdapter;
     private String getUserMobile = "";
+    private RecyclerView chattingRecyclerView;
+    private boolean LoadingFirstTime = true;
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://androidfirebasechat-6ee81-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +53,23 @@ public class Chat extends AppCompatActivity {
         messageET = findViewById(R.id.messageET);
         sendBtn = findViewById(R.id.sendBtn);
         profilePic = findViewById(R.id.profilePic);
+        chattingRecyclerView = findViewById(R.id.chattingRecyclerView);
 
         //get data from messages adapter class
-        final String getname = getIntent().getStringExtra("name");
+        final String getName = getIntent().getStringExtra("name");
         final String getProfile = getIntent().getStringExtra("profile_pic");
         chatKey = getIntent().getStringExtra("chat_key");
         final String getMobile = getIntent().getStringExtra("mobile");
         //get user mobile from memory
         getUserMobile = MemoryData.getData(Chat.this);
-        name.setText(getname);
+        name.setText(getName);
+
+        chattingRecyclerView.setHasFixedSize(true);
+        chattingRecyclerView.setLayoutManager(new LinearLayoutManager(Chat.this));
+        chatAdapter = new ChatAdapter(chatLists, Chat.this);
+        chattingRecyclerView.setAdapter(chatAdapter);
+
+
         if(getProfile.length()!=0){
             Picasso.get().load(getProfile).into(profilePic);
         }
@@ -56,15 +77,45 @@ public class Chat extends AppCompatActivity {
             profilePic.setImageResource(R.drawable.user_icon);
         }
 
-        try {
-            if (chatKey.isEmpty()) {
-                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         //generate chat key, by default is 1
-                        chatKey= "1";
-                        if(snapshot.hasChild("chat")){
-                            chatKey = String.valueOf(snapshot.child("chat").getChildrenCount() +1);
+                        if (chatKey.isEmpty()) {
+                            chatKey = "1";
+                            if (snapshot.hasChild("chat")) {
+                                chatKey = String.valueOf(snapshot.child("chat").getChildrenCount() + 1);
+                            }
+                        }
+                        if (snapshot.hasChild("chat")){
+
+                            if (snapshot.child("chat").child(chatKey).hasChild("messages")){
+                                chatLists.clear();
+                                for (DataSnapshot messageSnapshot : snapshot.child("chat").child(chatKey).child("messages").getChildren()){
+                                    if (messageSnapshot.hasChild("msg")&& messageSnapshot.hasChild("mobile")){
+                                        final String msgTimestamp = messageSnapshot.getKey();
+                                        final String getMobile = messageSnapshot.child("mobile").getValue(String.class);
+                                        final String getMsg = messageSnapshot.child("msg").getValue(String.class);
+                                        Timestamp timestamp = new Timestamp(Long.parseLong(msgTimestamp));
+                                        Date date = new Date(timestamp.getTime());
+                                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                                        SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("hh:mm aa", Locale.getDefault());
+
+
+                                        ChatList chatList = new ChatList(getMobile, getName, getMsg, simpleDateFormat.format(date),simpleTimeFormat.format(date));
+
+                                        chatLists.add(chatList);
+                                        if(LoadingFirstTime || Long.parseLong(msgTimestamp) > Long.parseLong(MemoryData.getLastMsgTS(Chat.this, chatKey))){
+                                            LoadingFirstTime = false;
+                                            MemoryData.saveLastMsgTS(msgTimestamp, chatKey, Chat.this);
+                                            chatAdapter.updateChatList(chatLists);
+
+                                            chattingRecyclerView.scrollToPosition(chatLists.size()-1);
+                                        }
+                                    }
+
+                                }
+                            }
                         }
                     }
 
@@ -73,10 +124,9 @@ public class Chat extends AppCompatActivity {
 
                     }
                 });
-            }
-        }catch (Exception e){
 
-        }
+
+
         backBtn.setOnClickListener(view ->{
             finish();
         });
